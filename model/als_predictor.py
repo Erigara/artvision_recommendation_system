c105e239be3a0ca4accd2575a0f075d4b72721a8
Erigara <shanin1000@yandex.ru>
2019-11-16 18:24:37 +0300

    Change interface to model, add struct representing data

--- model/als_predictor.py
+++ model/als_predictor.py
@@ -14,6 +14,7 @@ from model.utils.sparse_matrix_operations import (row_means_nonzero,
                                                   col_means_nonzero)
 from model.utils.metrics import reg_rmse, reg_se
 from model.utils.compute_matrix import ComputeMatrix
+from model import Losses
 
 import logging
 import time
@@ -25,7 +26,7 @@ class ALS:
     Implement Realization of ALS with Weighted-λ-Regularization from
     "Large-scale Parallel Collaborative Filtering for the Netflix Prize"
     """
-    def __init__(self, train_data, features=10, regularization='l2', lmbda = 0.3):
+    def __init__(self, features=10, regularization='l2', lmbda = 0.3):
         """
         train_data : RatingData
             rating data to train
@@ -42,7 +43,7 @@ class ALS:
         
         !!! To work properly ids must start form 0.
         """
-        
+
         if regularization == 'wl2':
             self.reg_penalty = self.weighted_reg_penalty
             self.regularization = 'wl2'
@@ -51,31 +52,50 @@ class ALS:
             self.regularization = 'l2'
         else:
             raise ValueError('regularization can be "l2" or "wl2" not "{}"'.format(regularization))
-        
-        self.train_data = train_data
-        self.users_len = int(max(train_data.df[train_data.user_col_name]) + 1)
-        self.items_len = int(max(train_data.df[train_data.item_col_name]) + 1)
+
         self.features = features
         self.lmbda = lmbda
-        # create and init matrices U and M
-        self.U = np.random.randn(self.features, self.users_len)
-        self.M = np.random.randn(self.features, self.items_len)
-        self.U, self.M = self.init_U(), self.init_M()
+        # matrix that represent users
+        self.U = None
+        # matrix that represent items
+        self.M = None
+        # additional data structures
+        self.TrainStruct = namedtuple('TrainStruct', ['user_I',
+                                                      'item_I',
+                                                      'users_len',
+                                                      'items_len'])
+    def predict(self, data):
+        """
+        Predict rating for each (user, item) indecies pair in users, items
 
-        user_groups = train_data.df.groupby(train_data.user_col_name)[train_data.item_col_name]
-        self.user_I = defaultdict(lambda : np.array([]))
-        for user in sorted(user_groups.groups):
-            self.user_I[user] = user_groups.get_group(user).values
+        data : RatingData
+            rating rata
+            
+        return : np.array
+            predicted ratings
+        """
+        if self.M is None or self.U is None:
+            logging.error('predictor must be fitted first!')
+            raise RuntimeError('predictor must be fitted first!')
+            
+        ratings_hat = []
+        for user, item in zip(data.df[data.user_col_name], data.df[data.item_col_name]):
+            try:
+                rating_hat = np.dot(self.U[:, user], self.M[:, item])
+            except IndexError:
+                logging.warning('invalid ids pair ({}, {}) in user_item_iterable'.format(user, item))
+                rating_hat = np.nan
+            ratings_hat.append(rating_hat)
+
+        rating_hat = np.array(ratings_hat)
         
-        
-        item_groups = train_data.df.groupby(train_data.item_col_name)[train_data.user_col_name]
-        self.item_I = defaultdict(lambda : np.array([], dtype=int))        
-        for item in sorted(item_groups.groups):
-            self.item_I[item] = item_groups.get_group(item).values
-        
-    def fit(self, test_data,
-                  test_metric=reg_rmse, 
-                  epochs=5, 
+        data.df[data.prediction_col_name] = rating_hat
+        return data
+            
+    def fit(self, train_data,
+                  test_data,
+                  test_metric=reg_rmse,
+                  epochs=5,
                   eps=0.001,
                   eval_every=1):
         """
@@ -94,7 +114,7 @@ class ALS:
         eps : float
             algorithm stops if difference between last_loss and loss smaller than eps
         """
-        Losses = namedtuple('Losses', ['train_losses', 'test_losses'])
+        train_struct = self.fit_init(train_data)
         train_losses = []
         test_losses = []
         last_loss = None
@@ -102,9 +122,9 @@ class ALS:
         train_start_time = time.time()
 
         for epoch in range(epochs):
-            self.step()
-            self.train_data = self.predict(self.train_data)
-            loss = reg_se(self.train_data)
+            self.optimization_step(train_data, train_struct)
+            self.train_data = self.predict(train_data)
+            loss = reg_se(train_data, penalty=self.reg_penalty(train_struct))
             
             if last_loss and abs(loss - last_loss) < eps:
                 last_loss = loss
@@ -136,106 +156,119 @@ class ALS:
         logging.info("Totall train time : {:.6f}  sec.".format(train_time))
 
         return Losses(train_losses, test_losses)
-
-    def step(self):
+    
+    def fit_init(self, train_data):
         """
-        Perform single optimization step op algorithm.
-        """
-        compute_U = ComputeMatrix(self.create_compute_u, self.features, self.users_len)
-        compute_M = ComputeMatrix(self.create_compute_m, self.features, self.items_len)
-
-        self.U = compute_U.compute_parallel()
-        self.M = compute_M.compute_parallel()
-
-    def predict(self, data):
-        """
-        Predicted rating for each (user, item) indecies pair in users, items
-
-        data : RatingData
-            rating rata
+        Initilize matices M, U using train data,
+        create additional data structures necessary for training predictor
+        
+        train_data : RatingData
+            rating data to train
             
-        return : np.array
-            predicted ratings
+        return : self.TrainStruct
+            namedtuple of additional data structures
         """
-        ratings_hat = []
-        
-        for user, item in zip(data.df[data.user_col_name], data.df[data.item_col_name]):
-            try:
-                rating_hat = np.dot(self.U[:, user], self.M[:, item])
-            except IndexError:
-                logging.warning('invalid ids pair ({}, {}) in user_item_iterable'.format(user, item))
-                rating_hat = np.nan
-            ratings_hat.append(rating_hat)
+        users_len = int(max(train_data.df[train_data.user_col_name]) + 1)
+        items_len = int(max(train_data.df[train_data.item_col_name]) + 1)
 
-        rating_hat = np.array(ratings_hat)
+        # init matrices U and M
+        self.U = self.init_U(train_data, users_len)
+        self.M = self.init_M(train_data, items_len)
         
-        data.df[data.prediction_col_name] = rating_hat
-        return data
+        # create user_I dict containing for key user np.array of items witch user rate
+        user_I = defaultdict(lambda : np.array([], dtype=int))
+        user_groups = train_data.df.groupby(train_data.user_col_name)[train_data.item_col_name]
+        for user in sorted(user_groups.groups):
+            user_I[user] = user_groups.get_group(user).to_numpy().astype(int)
+        
+        # create item_I dict containing for key item np.array of users whom rate this item
+        item_I = defaultdict(lambda : np.array([], dtype=int))
+        item_groups = train_data.df.groupby(train_data.item_col_name)[train_data.user_col_name]
+        for item in sorted(item_groups.groups):
+            item_I[item] = item_groups.get_group(item).to_numpy().astype(int)
+        
+        return self.TrainStruct(user_I, item_I, users_len, items_len)
 
-    def weighted_reg_penalty(self):
-        """
-        Compute weighted lambda regularization penalty
-        
-        return : float 
-            weighted lambda regularization penalty
-        """
-        # Tikhonov regularization matrices
-        Gu = self.U @ sps.diags([len(self.user_I[i]) for i in self.user_I])
-        Gm = self.M @ sps.diags([len(self.item_I[i]) for i in self.item_I])
-        
-        penalty = self.lmbda * ((Gu**2).sum() + (Gm**2).sum())
-        return penalty
-    
-    def l2_penalty(self):
-        """
-        Compute l2 regularization penalty
-        
-        return : float 
-            l2 penalty
-        """
-        penalty = self.lmbda * ((self.U**2).sum() + (self.M**2).sum())
-        return penalty
-    
-    def init_M(self):
-        """
-        Initialize matrix M so that it first row contain item's means
-        
-        return : np.array
-            initilized matrix M
-        """
-        col_R = sps.csc_matrix((self.train_data.df[self.train_data.rating_col_name], 
-                                (self.train_data.df[self.train_data.user_col_name], 
-                                 self.train_data.df[self.train_data.item_col_name])))
-        M = self.M.copy()
-        M[0, :] = col_means_nonzero(col_R)
-        return M
-    
-    def init_U(self):
+    def init_U(self, train_data, users_len):
         """
         Initialize matrix U so that it first row contain user's means
         
+        train_data : RatingData
+            rating data to train
+
+        items_len : int
+            amount of items
+
         return : np.array
             initilized matrix U
         """
-        row_R = sps.csr_matrix((self.train_data.df[self.train_data.rating_col_name], 
-                                (self.train_data.df[self.train_data.user_col_name], 
-                                 self.train_data.df[self.train_data.item_col_name])))
-        U = self.U.copy()
+        row_R = sps.csr_matrix((train_data.df[train_data.rating_col_name], 
+                                (train_data.df[train_data.user_col_name], 
+                                 train_data.df[train_data.item_col_name])))
+        U = np.random.randn(self.features, users_len)
         U[0, :] = row_means_nonzero(row_R)
         return U
+    
+    def init_M(self, train_data, items_len):
+        """
+        Initialize matrix M so that it first row contain item's means
+
+        train_data : RatingData
+            rating data to train
+
+        items_len : int
+            amount of items
+
+        return : np.array
+            initilized matrix M
+        """
+        col_R = sps.csc_matrix((train_data.df[train_data.rating_col_name], 
+                                (train_data.df[train_data.user_col_name], 
+                                 train_data.df[train_data.item_col_name])))
+
+        M = np.random.randn(self.features, items_len)
+        M[0, :] = col_means_nonzero(col_R)
+        return M
+
+    def optimization_step(self, train_data, train_struct):
+        """
+        Perform single optimization step op algorithm.
+        
+        train_data : RatingData
+            rating data to train
+            
+        train_struct : self.TrainStruct
+            namedtuple of additional data structures
+        """
+        compute_U = ComputeMatrix(lambda : self.create_compute_u(train_data, 
+                                                                 train_struct.user_I), 
+                                  self.features, 
+                                  train_struct.users_len)
+        compute_M = ComputeMatrix(lambda : self.create_compute_m(train_data, 
+                                                                 train_struct.item_I), 
+                                  self.features, 
+                                  train_struct.items_len)
+
+        self.U = compute_U.compute_parallel()
+        self.M = compute_M.compute_parallel()
   
-    def create_compute_u(self):
+    def create_compute_u(self, train_data, user_I):
         """
         Create function to compute column of U by it's index
         
+        train_data : RatingData
+            rating data to train
+        
+        user_I : dict
+            dict containing for key user np.array of items witch user rate
+            
         return : function
             function to compute columns of U
         """
         M = self.M
-        row_R = sps.csr_matrix((self.train_data.df[self.train_data.rating_col_name], 
-                                (self.train_data.df[self.train_data.user_col_name], 
-                                 self.train_data.df[self.train_data.item_col_name])))
-        user_I = self.user_I
+        row_R = sps.csr_matrix((train_data.df[train_data.rating_col_name], 
+                                (train_data.df[train_data.user_col_name], 
+                                 train_data.df[train_data.item_col_name])))
         E = np.eye(self.features)
         lmbda = self.lmbda
         
@@ -259,18 +292,23 @@ class ALS:
         
         return compute_u
     
-    def create_compute_m(self):
+    def create_compute_m(self, train_data, item_I):
         """
         Create function to compute column of M by it's index
         
+        train_data : RatingData
+            rating data to train
+        
+        item_I : dict 
+            dict containing for key item np.array of users whom rate this item
+            
         return : function
             function to compute columns of M
         """
         U = self.U        
-        col_R = sps.csc_matrix((self.train_data.df[self.train_data.rating_col_name], 
-                                (self.train_data.df[self.train_data.user_col_name], 
-                                 self.train_data.df[self.train_data.item_col_name])))
-        item_I = self.item_I
+        col_R = sps.csc_matrix((train_data.df[train_data.rating_col_name], 
+                                (train_data.df[train_data.user_col_name], 
+                                 train_data.df[train_data.item_col_name])))
         E = np.eye(self.features)
         lmbda = self.lmbda
         
@@ -292,3 +330,34 @@ class ALS:
             m_j = np.linalg.solve(A_j, V_j)
             return m_j.flatten()
         return compute_m
+    
+    def weighted_reg_penalty(self, train_struct):
+        """
+        Compute weighted lambda regularization penalty
+        
+        train_struct : self.TrainStruct
+            namedtuple of additional data structures
+            
+        return : float 
+            weighted lambda regularization penalty
+        """
+        user_I, item_I = train_struct.user_U, train_struct.item_I
+        # Tikhonov regularization matrices
+        Gu = self.U @ sps.diags([len(user_I[i]) for i in user_I])
+        Gm = self.M @ sps.diags([len(item_I[i]) for i in item_I])
+        
+        penalty = self.lmbda * ((Gu**2).sum() + (Gm**2).sum())
+        return penalty
+    
+    def l2_penalty(self, train_struct):
+        """
+        Compute l2 regularization penalty
+
+        train_struct : self.TrainStruct
+            namedtuple of additional data structures
+
+        return : float 
+            l2 penalty
+        """
+        penalty = self.lmbda * ((self.U**2).sum() + (self.M**2).sum())
+        return penalty
--- model/baseline_predictor.py
+++ model/baseline_predictor.py
@@ -9,23 +9,41 @@ Module implement unpersonalized predictor based on mean and user's/item's deviat
 from multiprocessing import Pool
 import pandas as pd
 import numpy as np
+from model.utils.metrics import reg_rmse, reg_se
+from model import Losses
+
 import logging
+
 class BaselinePredictor:
     """
     Based on mean, user_means, item_means compute predicted rating
     rating(i, j) = user_means(i) + item_mean(j) - mean
     
     """
-    def __init__(self, train_data):
+    def __init__(self):
         """
         Set predictor parameters
     
         train_data : RatingData
             rating data to train
         """
+        self.mean = None
+        self.user_means = None
+        self.item_means = None
+    
+    def fit(self, train_data, test_data, test_metric=reg_rmse):
         self.mean = train_data.df[train_data.rating_col_name].mean()
-        self.user_means = np.array(train_data.df.groupby(train_data.user_col_name).mean())
-        self.item_means = np.array(train_data.df.groupby(train_data.item_col_name).mean())
+        self.user_means = train_data.df.groupby(
+                train_data.user_col_name).mean()[train_data.rating_col_name]
+        self.item_means = train_data.df.groupby(
+                train_data.item_col_name).mean()[train_data.rating_col_name]
+        
+        train_loss = reg_se(self.predict(train_data))
+        test_data = self.predict(test_data)
+        test_data.df[test_data.prediction_col_name].fillna(0)
+        test_loss =  test_metric(test_data
+                                 )
+        return Losses([train_loss,], [test_loss,])
     
     def predict_single(self, user, item):
         """
@@ -57,6 +75,10 @@ class BaselinePredictor:
         return : np.array
             list of baseline_ratings
         """
+        if self.mean is None or self.item_means is None or self.user_means is None:
+            logging.error('predictor must be fitted first!')
+            raise RuntimeError('predictor must be fitted first!')
+            
         with Pool() as p:
             data.df[data.prediction_col_name] = np.fromiter(p.starmap(self.predict_single, 
                                                                       zip(data.df[data.user_col_name], 
