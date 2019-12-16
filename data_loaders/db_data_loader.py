98bd17f3ec7cb2523b02b5b35337a99d328fd5b8
Erigara <shanin1000@yandex.ru>
2019-12-16 15:41:40 +0300

    Generic connection to db

--- data_loaders/db_data_loader.py
+++ data_loaders/db_data_loader.py
@@ -4,6 +4,7 @@
 @author: erigara
 """
 import psycopg2
+from psycopg2 import sql
 import pandas as pd
 import sys
 sys.path.append('/data/artvision/recommendation_system')
@@ -16,31 +17,33 @@ DEC2FLOAT = psycopg2.extensions.new_type(
     lambda value, curs: float(value) if value is not None else None)
 psycopg2.extensions.register_type(DEC2FLOAT)
 
-def load_data(path, names):
+def load_data(host, port, dbname, table, columns, user, password):
     '''
-    Read from db connection, create new sequential indeces for users and items
+    Read rating data from db connection
+
     
-    path : str
-        path of csv file
-    
-    names : array-like
+    columns : array-like
         column names to use, 
-        column names must be in following order: [user_ids, item_ids, rating, prediction, timestamp] 
+        column names must be in following order: [user_id, item_id, rating, prediction, timestamp] 
     
     return : RatingData
         dowloaded rating data
     '''
-    conn = psycopg2.connect(dbname='rating_db', user='postgres',  host='localhost')
+    conn = psycopg2.connect(host=host,
+                            port=port,
+                            dbname=dbname,
+                            user=user,
+                            password=password)
     cursor = conn.cursor(name='record_fetcher')
     cursor.itersize = 20000
-    cursor.execute('SELECT userId, movieId, rating, timestamp FROM ratings')
-    df = pd.DataFrame(data=cursor, columns=[names[0], names[1], names[2], names[4]])
+    template_query = sql.SQL("SELECT {user_id}, {item_id}, {rating}, {timestamp} FROM {table}")
+    query = template_query.format(user_id=sql.Identifier(columns[0]),
+                                  item_id=sql.Identifier(columns[1]),
+                                  rating=sql.Identifier(columns[2]),
+                                  timestamp=sql.Identifier(columns[4]),
+                                  table=sql.Identifier(table))
+    cursor.execute(query)
+    df = pd.DataFrame(data=cursor, columns=[columns[0], columns[1], columns[2], columns[4]])
     conn.close()
-    # create new sequential indeces for users and items
-    '''
-    user_ids, item_ids = 'new_' + names[0], 'new_' + names[1]
-    df[user_ids] = df[names[0]].rank(method='dense').astype('int64') - 1
-    df[item_ids] = df[names[1]].rank(method='dense').astype('int64') - 1   
-    names[0], names[1] = user_ids, item_ids
-    '''
-    return RatingData(df, *names)
+    
+    return RatingData(df, *columns)
